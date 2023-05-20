1c493990c31cb9363840c5dd092932fa6a091d9d
Faizul-Hassan-2015 <86608620+Faizul-Hassan-2015@users.noreply.github.com>
2023-05-20 19:10:46 +0600

    terms and condiotion and privacy policy page updated

--- src/Components/PrivacyPolicy/PrivacyPolicy.js
+++ src/Components/PrivacyPolicy/PrivacyPolicy.js
@@ -8,163 +8,124 @@ function PrivacyPolicy(props) {
     <div className="container mx-auto text-black">
       <div className="privacy">
         <div className="container">
-          <div className="row mt-5">
+          <div className="mx-10 mt-5">
             <img src={privacy} width="100%" height="100%" alt="" />
 
-            <h1 className="text-center mt-5 text-4xl font-semibold">
+            <h1 className="text-center mt-5 mb-5 text-4xl font-semibold">
               PRIVACY POLICY
             </h1>
             <div>
+              <p className="pb-5">
+                At Fry Frenzy, we are committed to protecting your privacy. This
+                Privacy Policy outlines how we collect, use, and safeguard your
+                personal information when you visit our website. By using our
+                website, you consent to the practices described in this policy.
+              </p>
               <b style={{ fontSize: "20px", marginTop: "3%" }}>
                 {" "}
-                Brief Introduction{" "}
+                1 Information We Collect
               </b>
               <p>
-                We know that you care how information about you is used and
-                shared, and we appreciate your trust. This Privacy Policy
-                explains how The KOW Company collect, use and share information
-                about you when you use our websites and online services
-                (“Services”) or otherwise interact with us. Please read this
-                Privacy Policy carefully and contact us if you have any
-                questions.
+                1.1 Personal Information: We may collect personal information
+                such as your name, email address, phone number, and delivery
+                address when you voluntarily provide it to us through our
+                website.
                 <br />
-                If you provide us with your personal information then this
-                indicates that you have had sufficient opportunity to access,
-                and have read and accepted, this Privacy Policy. If you do not
-                wish to provide personal information to us, then you do not have
-                to do so, however it may affect your use of this website or any
-                products and services offered on it.{" "}
+                1.2 Usage Information: We may collect non-personal information
+                about your interactions with our website, including your IP
+                address, browser type, operating system, and referring website.
               </p>
             </div>
 
             <div className="mt-6">
-              <b style={{ fontSize: "20px" }}>
-                {" "}
-                What Information we are Collecting from you?{" "}
-              </b>
-              <br />
-
-              <ul>
-                <li>
-                  Contact details including Name, email address, address and
-                  telephone number{" "}
-                </li>
-                <li>
-                  Demographic information such as Job Title, Gender, postcode,
-                  company name, company type{" "}
-                </li>
-                <li>
-                  Payment Details like Credit Card Information, Account
-                  Information, Billing Address Information (if you are buying
-                  product from us);{" "}
-                </li>
-                <li> Raw Images provided by you to us </li>
-                <li>SOP for Image Processing </li>
-                <li>Your Computers IP Address </li>
-                <li>Browser Type </li>
-              </ul>
-            </div>
-
-            <div className="mt-6">
-              <b style={{ fontSize: "20px" }}>
-                How may we use your personal information?{" "}
-              </b>
-              <br />
-
-              <ul>
-                <li> To Create orders </li>
-                <li> Agreements for the sale of products or services </li>
-                <li>
-                  {" "}
-                  Keeping transaction records accounts, tax invoices or receipts{" "}
-                </li>
-                <li>
-                  {" "}
-                  For communication Purpose (about products and services,
-                  special offers, and events which might interest you){" "}
-                </li>
-                <li>
-                  {" "}
-                  For performing research and analysis to improve our product
-                  and service quality to get your requirement with perfection.{" "}
-                </li>
-                <li> To deliver personalized or customized content to you </li>
-                <li> For Direct Marketing </li>
-                <li> To offer additional benefits to you </li>
-                <li> To send you promotional information </li>
-                <li>
-                  {" "}
-                  To comply with laws or regulations or to comply with any
-                  directions given by regulators or authorities.{" "}
-                </li>
-              </ul>
-            </div>
-
-            <div className="mt-6">
-              <b style={{ fontSize: "20px" }}>
-                Disclosure of Personal Information to Third Parties?{" "}
-              </b>
+              <b style={{ fontSize: "20px" }}> 2 Use of Information</b>
               <br />
 
               <p>
-                {" "}
-                We may disclose your personal information to:
-                <ul>
-                  <li>
-                    Credit reporting agencies and courts, tribunals and
-                    regulatory authorities where customers fail to pay for goods
-                    or services provided by us to them{" "}
-                  </li>
-                  <li>
-                    Courts, tribunals, regulatory authorities and law
-                    enforcement offices as required by law, in connection with
-                    any actual or prospective legal proceedings, or in order to
-                    establish, exercise or defend our legal rights
-                  </li>
-                  <li>
-                    Conducting market research and marketing strategy analysis{" "}
-                  </li>
-                </ul>
-                Where we disclose your personal information to third parties for
-                these purposes, we will request that they follow this Privacy
-                Policy when handling your personal information. If there is a
-                change of control of our business or a sale or transfer of
-                business assets, we reserve the right to transfer to the extent
-                permitted by law our user databases, together with any personal
-                information and non-personal information contained in those
-                databases.{" "}
+                2.1 Personal Information: We may use your personal information
+                to process and fulfill your orders, communicate with you
+                regarding your orders or inquiries, and provide customer
+                support.
+              </p>
+              <p>
+                2.2 Usage Information: We may use non-personal information to
+                analyze trends, administer the website, track users' movements,
+                and gather demographic information for aggregate use. This
+                information helps us improve our website and tailor it to our
+                users' needs.
+              </p>
+            </div>
+
+            <div className="mt-6">
+              <b style={{ fontSize: "20px" }}>3 Security</b>
+              <br />
+
+              <p>
+                We are committed to ensuring the security of your personal
+                information. We implement reasonable security measures to
+                protect against unauthorized access, alteration, disclosure, or
+                destruction of your personal information.
+              </p>
+            </div>
+
+            <div className="mt-6">
+              <b style={{ fontSize: "20px" }}>4 Third-Party Disclosure</b>
+              <br />
+
+              <p>
+                We do not sell, trade, or transfer your personal information to
+                outside parties unless we provide you with advance notice.
+                However, this does not include trusted third parties who assist
+                us in operating our website, conducting our business, or serving
+                you, as long as those parties agree to keep your information
+                confidential.
               </p>
             </div>
 
             <div className="mt-5">
-              <b style={{ fontSize: "20px" }}>
-                {" "}
-                How to make Complaint about a breach in privacy?{" "}
-              </b>
+              <b style={{ fontSize: "20px" }}> 5 Cookies</b>
               <br />
 
               <p>
-                If you have any questions, comments or complaints regarding our
-                practices, or if you are of the view that we have not adhered to
-                this privacy policy, you can contact us by email
-                to eat@fryfrenzy.com
+                Our website may use cookies to enhance your browsing experience.
+                Cookies are small text files stored on your device that allow
+                the website to remember your preferences and improve
+                performance. You can choose to accept or decline cookies. Please
+                refer to our Cookie Policy for more details.
               </p>
             </div>
             <div className="mt-5">
               <b style={{ fontSize: "20px" }}>
-                Changes/Amendments to this Policy{" "}
+                6 Links to Third-Party Websites
               </b>
               <br />
 
               <p>
-                This Privacy Policy may be amended, including with changes,
-                additions and deletions, from time to time in our sole
-                discretion. Your continued use of our website following any
-                amendments indicates that you accept the amendments. We
-                recommend that you check this Privacy Policy regularly, prior to
-                providing personal information, to ensure you are aware of any
-                changes, and only proceed to provide personal information if you
-                accept the new Privacy Policy.{" "}
+                Our website may contain links to third-party websites. We are
+                not responsible for the privacy practices or content of such
+                websites. We encourage you to review the privacy policies of
+                those websites before providing any personal information.
+              </p>
+            </div>
+            <div className="mt-5">
+              <b style={{ fontSize: "20px" }}>
+                7 Changes to this Privacy Policy
+              </b>
+              <br />
+
+              <p>
+                We reserve the right to modify this Privacy Policy at any time.
+                Any changes will be posted on this page, and the updated policy
+                will be effective immediately upon posting.
+              </p>
+            </div>
+            <div className="mt-5">
+              <b style={{ fontSize: "20px" }}>8 Contact Us</b>
+              <br />
+
+              <p>
+                If you have any questions or concerns regarding this Privacy
+                Policy, please contact us at eat@fryfrenzy.com
               </p>
             </div>
           </div>
--- src/Components/TermsCondition/TermsCondition.js
+++ src/Components/TermsCondition/TermsCondition.js
@@ -5,208 +5,82 @@ import terms from "../../images/terms-banner.jpg";
 
 function TermsCondition(props) {
   return (
-    <div className="terms container mx-auto text-black">
-      <div className="container">
-        <div className="row mt-5 mb-5">
-          <img src={terms} width="100%" height="100%" alt="" />
-          <h1 className="text-center text-4xl font-semibold mt-3">
-            Terms & Conditions
-          </h1>
-          <br />
-          <br />
-          <div>
-            <b style={{ fontSize: "20px" }}> Placing an Order </b>
-
-            <ul>
-              <li>
-                By Uploading your Images in your FTP Server and send the link to
-                us
-              </li>
-              <li>
-                By uploading images in our FTP server through the link provided
-                by us and notify us
-              </li>
-              <li>
-                By Uploading Images through our Web App and Shopify App from
-                your Local Storage (Laptop, Personal Computer etc.)
-              </li>
-            </ul>
-          </div>
-
-          <div className="mt-5">
-            <b style={{ fontSize: "20px" }}> Payment Method </b>
-
-            <ul>
-              <li>
-                Need to Advance 30% of the payment after placing the order as
-                advance.
-              </li>
-              <li>
-                Need to Pay 30% of the payment after getting the Work in
-                Progress (Processed images as per the SOP)
-              </li>
-              <li>
-                Rest of the 40% should be paid within 1 week of getting the
-                Finished Goods (Processed Images after Final Correction)
-              </li>
-              <li>
-                Payment can be made by Visa, MasterCard, Amex, only. You warrant
-                that you are an authorized user of the credit card and other
-                accounts used to complete payment.
-              </li>
-              <li>
-                All or any of the above can be waived upon discussion with our
-                client management team.
-              </li>
-            </ul>
-          </div>
-
-          <div className="mt-5">
-            <b style={{ fontSize: "20px" }}> Order Cancellation Policy </b>
-
-            <ul>
-              <li>
-                Order is needed to be cancelled before we process 10 images from
-                the images you provide us. If you cancel within this limit you
-                will get back the amount you have paid as advance.
-              </li>
-              <li>
-                If you cancelled your order after processing 10 images than you
-                will not get any payback for your advance payment.
-              </li>
-              <li>
-                If you cancelled order after work in progress than you need to
-                pay the 60% of the Total Payment
-              </li>
-              <li>
-                After getting the Finished Good there will be no order
-                cancellation policy.
-              </li>
-            </ul>
-          </div>
-
-          <div className="mt-5">
-            <b style={{ fontSize: "20px" }}>Faulty Policy </b>
-
-            <p>
-              We aim to provide you with products of the highest standard and
-              quality. That’s why we have a Work in Progress stage so that you
-              can evaluate our work carefully. You will get 48 Hours to evaluate
-              Work in Progress. If there are fault or faults in our work than we
-              will correct it as per your observation. Than you will get another
-              chance to give your feedback or observation to recover the error.
-              After that we will send you the finished goods. Once you give the
-              second feedback, there will be no option for order cancellation or
-              any other observation.
-            </p>
-          </div>
-
-          <div className="mt-5">
-            <b style={{ fontSize: "20px" }}> Limitation of Liability </b>
-
-            <p>
-              We take all precautions possible to keep the details of your
-              orders and payment secure, but in the absence of negligence on our
-              part, we cannot be held liable for any loss you may suffer if a
-              third party acquires unauthorized access to any data you provide
-              when accessing or ordering from the website.
-            </p>
-          </div>
-
-          <div className="mt-5">
-            <b style={{ fontSize: "20px" }}>Intellectual Property </b>
-
-            <p>
-              Fry Frenzy guarantees that it has not infringed or does not
-              infringe or shall not infringe the IPR of any third party so far
-              as related with the service(s) to be provided. The KOW Company
-              shall indemnify its Clients against any costs or consequences as a
-              result of such infringements.
-            </p>
-          </div>
-
-          <div className="mt-5">
-            <b style={{ fontSize: "20px" }}>Severability </b>
-
-            <p>
-              If any portion of the order is determined to be enforceable, the
-              remainder of the order shall continue in full force and effect,
-              and either party may renegotiate the terms affected by the
-              severance.
-            </p>
-          </div>
-
-          <div className="mt-5">
-            <b style={{ fontSize: "20px" }}>Governing Law </b>
-
-            <p>
-              The whole process shall be governed and construed in accordance
-              with the laws of Bangladesh.
-            </p>
-          </div>
-
-          <div className="mt-5">
-            <b style={{ fontSize: "20px" }}>Settlement of Dispute </b>
-
-            <p>
-              We will give our best efforts to settle amicably all disputes
-              arising in connection with our clients. There will be 15 days’
-              time to settle the dispute after the first written notice from the
-              either parties.  If both of us are failed to an amicable
-              settlement within 15 (Fifteen) days of the first written notice
-              from either Party on the matter of disagreement, the parties shall
-              refer the dispute to any competent court of this jurisdiction.
-            </p>
-          </div>
-
-          <div className="mt-5">
-            <b style={{ fontSize: "20px" }}>Assignment </b>
-
-            <p>
-              Fry Frenzy cannot use a third party without taking the
-              consent of the client.
-            </p>
-          </div>
-
-          <div className="mt-5">
-            <b style={{ fontSize: "20px" }}>Notice and Communication </b>
-
-            <p>
-              All Communications should be done by the registered email address
-              of the authorized representatives from both parties. The receiving
-              party if so requested by the sending party without undue delay
-              shall confirm any correspondence.
-            </p>
-          </div>
-
-          <div className="mt-5">
-            <b style={{ fontSize: "20px" }}>Confidentiality </b>
-
-            <p>
-              Fry Frenzy may need to disclose certain “Confidential
-              Information” to its sister concerns to some extent as required.
-              Confidential Information shall mean any information disclosed by a
-              Party in connection with this Contract to another party (whether
-              oral or written).
-              <br />
-              <br />
-              Parties agree to hold all Confidential Information of the other in
-              trust and confidence, and to protect it to the same extent as it
-              would protect its own Confidential Information (which shall in any
-              event be reasonable protection) and not to use such Confidential
-              Information for other purposes. Unless disclosure is in compliance
-              with a valid order from a court of competent jurisdiction, law
-              enforcing agency or other relevant regulatory bodies or otherwise
-              required by operations of the law or agreed by the other Party in
-              writing, neither Party shall disclose any Confidential Information
-              of the other Party, by publication or otherwise, to any person
-              other than its employees and contractors bound to confidential
-              obligations consistent with those set forth herein and who have a
-              need to know such Confidential Information for purposes of
-              exercising its rights.
-            </p>
-          </div>
-        </div>
+    <div className="pb-10 container mx-auto text-black">
+      <div className="row mt-5 mb-5">
+        <img src={terms} width="100%" height="100%" alt="" />
+        <h1 className="text-center text-4xl font-semibold mt-3">
+          Terms & Conditions
+        </h1>
+        <br />
+      </div>
+      <div className="mx-10 flex flex-col text-lg gap-3">
+        <p>
+          Welcome to the Fry Frenzy website. These Terms and Conditions outline
+          the rules and regulations for using our website. By accessing this
+          website, we assume you accept these terms and conditions in full. Do
+          not continue to use the Fry Frenzy website if you do not agree to all
+          the terms and conditions stated below.
+        </p>
+        <p>
+          <span className="font-bold">1.</span> The term 'Fry Frenzy' or 'us' or
+          'we' refers to the owner of the website. The term 'you' refers to the
+          user or viewer of our website.
+        </p>
+        <p>
+          {" "}
+          <span className="font-bold">2.</span> The content of this website is
+          for your general information and use only. It is subject to change
+          without notice.
+        </p>
+        <p>
+          <span className="font-bold">3.</span> This website uses cookies to
+          monitor browsing preferences. By using this website, you consent to
+          our use of cookies in accordance with our Privacy Policy.
+        </p>
+        <p>
+          <span className="font-bold">4.</span> Neither we nor any third parties
+          provide any warranty or guarantee as to the accuracy, timeliness,
+          performance, completeness, or suitability of the information and
+          materials found or offered on this website for any particular purpose.
+          You acknowledge that such information and materials may contain
+          inaccuracies or errors, and we expressly exclude liability for any
+          such inaccuracies or errors to the fullest extent permitted by law.{" "}
+        </p>
+        <p>
+          <span className="font-bold">5.</span> Your use of any information or
+          materials on this website is entirely at your own risk, for which we
+          shall not be liable. It is your responsibility to ensure that any
+          products, services, or information available through this website meet
+          your specific requirements.{" "}
+        </p>
+        <p>
+          <span className="font-bold">6.</span> This website contains material
+          that is owned by or licensed to us. This material includes, but is not
+          limited to, the design, layout, look, appearance, and graphics.
+          Reproduction is prohibited unless prior written consent is obtained.{" "}
+        </p>
+        <p>
+          {" "}
+          <span className="font-bold">7.</span>All trademarks reproduced on this
+          website, which are not the property of, or licensed to, the operator,
+          are acknowledged on the website.
+        </p>
+        <p>
+          <span className="font-bold">8.</span> Unauthorised use of this website
+          may give rise to a claim for damages and/or be a criminal offense.
+        </p>
+        <p>
+          <span className="font-bold">9.</span> From time to time, this website
+          may also include links to other websites. These links are provided for
+          your convenience to provide further information. They do not signify
+          that we endorse the website(s). We have no responsibility for the
+          content of the linked website(s).
+        </p>
+        <p>
+          <span className="font-bold">10.</span> Your use of this website and
+          any dispute arising out of such use of the website is subject to the
+          laws of Bangladesh.
+        </p>
       </div>
     </div>
   );
