07165917fa2a7c62762c4cb57c86705d4c737088
faizulhassanRien <faizulhassan.rien@gmail.com>
2023-09-01 14:58:21 +0600

    change reviews design

--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -4,8 +4,7 @@ const Review = ({ review }) => {
   return (
     <div className="card w-80 bg-base-100 shadow-xl">
       <div className="card-body">
-        <p className="mb-4">{review.review}</p>
-        <div className="flex  items-center">
+        <div className="flex  items-center mb-4">
           <div className="avatar">
             <div className="w-16 rounded-full ring ring-orange-400 ring-offset-base-100 ring-offset-2">
               <img src={review.img} alt="" />
@@ -16,6 +15,8 @@ const Review = ({ review }) => {
             <p className="ml-4">{review.location}</p>
           </div>
         </div>
+        <p className="">{review.review}</p>
+
       </div>
     </div>
   );
