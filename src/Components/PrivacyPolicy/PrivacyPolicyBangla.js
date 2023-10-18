027eb076d06801e0a643d31d2a9088ec9723e69d
faizulhassanRien <faizulhassan.rien@gmail.com>
2023-10-18 16:41:51 +0600

    refactor 3 pages

--- src/Components/PrivacyPolicy/PrivacyPolicyBangla.js
+++ src/Components/PrivacyPolicy/PrivacyPolicyBangla.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 
-import { Link } from "react-router-dom";
+
 import privacy from "../../images/privacy.jpg";
 
 function PrivacyPolicyBangla(props) {
